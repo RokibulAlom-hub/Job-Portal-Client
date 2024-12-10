b9a7511e9fde7ab696334c3755a4f47a3e93ce0a
Rokibul Alom <itismehey975@gmail.com>
2024-12-11 00:56:34 +0600

    reactrouter, tailwind css, daisy ui and the router path setup

---
+++ src/Router/Mainroute.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { createBrowserRouter } from 'react-router-dom';
+import MainLayout from '../Layouts/MainLayout';
+
+export const router = createBrowserRouter([
+    {
+        path:'/',
+        element:<MainLayout></MainLayout>
+    }
+])
